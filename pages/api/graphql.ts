29d27b3bd8fb030f483e3919533c14fb1663bf3c
Emmanuel <eagomez2387@gmail.com>
2023-10-08 16:12:23 -0500

    Add new commit to back

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,16 +1,23 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { prisma } from "../../prisma/db";
+import {} from "../../graphql/";
 
 import { PrismaClient } from "@prisma/client";
+import { resolvers } from "../../graphql";
+import { buildSchema } from "type-graphql";
 
 export type Context = {
   prisma: PrismaClient;
 };
 
+const schema = await buildSchema({
+  resolvers,
+  validate: false,
+});
+
 const apolloServer = new ApolloServer<Context>({
-  // typeDefs
-  // resolvers
+  schema,
 });
 
 export default startServerAndCreateNextHandler(apolloServer, {
